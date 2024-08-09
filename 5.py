a326de19674bcdf113fca61b6d148aa709d40c6d
Liza-040 <04042006liza@gmail.com>
2024-08-09 21:41:01 +0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+first = 7
+second = 7
+third = 6
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
